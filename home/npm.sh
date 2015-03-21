45c2cd8e0be18b31497d10ce4f9bedaaddffd74a
Naoki Endoh <naoki.endoh@gmail.com>
2015-03-21 11:11:42 +0900

    Add node modules install

---
+++ home/npm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+nodebrew install-binary stable
+nodebrew use stable
+
+node -v
+
+npm install -g babel
+npm install -g bower
+npm install -g browser-sync
+npm install -g browserify
+npm install -g coffee-script
+npm install -g cordova
+npm install -g generator-bespoke
+npm install -g grunt-cli
+npm install -g gulp
+npm install -g ionic
+npm install -g waatchify
+npm install -g webpack
+npm install -g yo
