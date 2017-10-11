e7dfaa090e0eb5cf51af0c91cd673622496dd6ad
Naoki Endoh <naokie@users.noreply.github.com>
2017-10-12 00:25:04 +0900

    Add pip for neovim

--- home/bootstrap.sh
+++ home/bootstrap.sh
@@ -15,4 +15,6 @@ curl -s "https://get.sdkman.io" | bash;
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh "$HOME/.cache/dein"
 
+pip3 install neovim
+
 ln -s /Applications/Visual\ Studio\ Code\ -\ Insiders.app/Contents/Resources/app/bin/code $HOME/bin/code
