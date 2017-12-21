80a8f5c6d03bfeda6d067bdc008cc7b5a8350579
Naoki Endoh <naoki.endoh@gmail.com>
2017-12-21 11:57:57 +0900

    Add npm alias

--- home/.config/fish/aliases.fish
+++ home/.config/fish/aliases.fish
@@ -10,7 +10,7 @@ function grep ; command grep --color=auto $argv ; end
 alias dl="cd ~/Downloads"
 alias dt="cd ~/Desktop"
 alias a="atom-beta"
-alias g="hub"
+alias g="git"
 alias v="nvim"
 alias h="history"
 alias j="jobs"
@@ -33,3 +33,16 @@ alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 
 # Reload the shell (i.e. invoke as a login shell)
 alias reload="exec $SHELL -l"
+
+# npm
+alias ni='npm install'
+alias nid='npm install --save-dev'
+alias nig='npm install --global'
+alias nt='npm test'
+alias nit='npm install; and npm test'
+
+alias nk='npm link'
+alias nr='npm run'
+alias ns='npm start'
+alias nf='npm cache clean; and rm -rf node_modules; and npm install'
+alias nlg='npm list --global --depth=0'
