b05912b8bfa111dfc8d4a8b4632dbc9fea599e2d
Naoki Endoh <naoki.endoh@gmail.com>
2020-11-03 13:50:17 +0900

    Update global npm

--- home/npm.sh
+++ home/npm.sh
@@ -2,25 +2,29 @@
 
 npm install -g npm
 
-npm install create-react-app
-npm install create-dmg
+# npm install create-react-app
+# npm install create-dmg
 # npm install -g git-open
 # npm install -g git-recent
-npm install -g diff-so-fancy
+# npm install -g diff-so-fancy
 npm install -g eslint
-npm install -g eslint-config-google
-npm install -g eslint-config-xo
-npm install -g eslint-plugin-import
-npm install -g eslint-plugin-json
-npm install -g eslint-plugin-optimize-regex
-npm install -g eslint-plugin-promise
-npm install -g eslint-plugin-unicorn
-# npm install -g flow-bin
+# npm install -g eslint-config-google
+# npm install -g eslint-config-xo
+# npm install -g eslint-plugin-import
+# npm install -g eslint-plugin-json
+# npm install -g eslint-plugin-optimize-regex
+# npm install -g eslint-plugin-promise
+# npm install -g eslint-plugin-unicorn
+npm install -g firebase-tools
 npm install -g gulp-cli
+npm install -g now
 npm install -g npm-check-updates
 npm install -g prettier
+npm install -g rimraf
 npm install -g svgo
-npm install -g trash-cli
+# npm install -g trash-cli
 npm install -g typescript
-npm install -g @vue/cli
+npm install -g tslint
+npm install -g ts-node
+# npm install -g @vue/cli
 # npm install -g yarn
